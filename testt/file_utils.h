c59a61a997ea883c85ac287f15dedef7fe4633a4
CihanC1 <cihanlinx@gmail.com>
2024-12-30 21:54:46 +0100

    Initial commit for test project

---
+++ testt/file_utils.h
@@ -0,0 +1,8 @@
+#ifndef FILE_UTILS_H
+#define FILE_UTILS_H
+
+void readFile(const char *filename);
+void writeFile(const char *filename, const char *content);
+
+#endif // FILE_UTILS_H
+
---
+++ testt/src/main.c
@@ -0,0 +1,9 @@
+#include "file_utils.h"
+
+int main() {
+    // Example usage of file_utils functions
+    readFile("example.txt");
+    writeFile("output.txt", "Hello, Cihan!");
+    return 0;
+}
+
