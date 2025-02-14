f13163f3dc9bc72195aeb5d9ea39c5b639fe2d64
Ay <k65597@fiw-w031bi.fhws.de>
2025-02-14 16:46:40 +0100

    add make test

--- project/modules/thread.h
+++ project/modules/thread.h
@@ -2,6 +2,7 @@
 #define THREAD_H
 
 #include <pthread.h>
+#include "args_parser.h"  // ✅ Arguments yapısını tanımlamak için ekledik
 
 #define MAX_THREADS 8
 
@@ -11,13 +12,14 @@ typedef struct {
 } thread_data_t;
 
 typedef struct {
-    thread_data_t *tasks;
-    pthread_t threads[MAX_THREADS];
-    int task_count;
-    int task_capacity;
-    int shutdown;
+    pthread_t *threads;  // Thread dizisini tanımla
     pthread_mutex_t mutex;
     pthread_cond_t cond;
+    thread_data_t *tasks;
+    int task_count;
+    int thread_count;
+    int task_capacity;
+    int shutdown;
     Arguments *args;
 } thread_pool_t;
 
@@ -27,4 +29,3 @@ void thread_pool_destroy(thread_pool_t *pool);
 void *worker_thread(void *arg);
 
 #endif // THREAD_H
-
